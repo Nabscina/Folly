3ebf85fa27b6b17dfd088cb83730f69245ac5bfc
Pebe <Nabscina@users.noreply.github.com>
2017-01-23 19:41:11 +0100

    luotiin mahdollisesti hyödyllisiä luokkia ja follylle vastausattribuutti

--- folly/src/main/java/follybot/follybot/Folly.java
+++ folly/src/main/java/follybot/follybot/Folly.java
@@ -3,15 +3,26 @@ package follybot.follybot;
 public class Folly {
 
     private String name;
+    private String answer;
 
     public Folly() {
 
         this.name = "folly";
+        this.answer = "";
     }
-    
+
     public String getName() {
-        
+
         return this.name;
     }
 
+    public void setAnswer(String a) {
+
+        this.answer = a;
+    }
+
+    public String respond(String answer) {
+
+        return "Folly: " + answer;
+    }
 }
