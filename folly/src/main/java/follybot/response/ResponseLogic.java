077b12eaf4d62f1de73724688f9c7ce7ff78ac50
Pebe <Nabscina@users.noreply.github.com>
2017-01-25 17:39:55 +0100

    follylta pois turha nimi-attribuutti

--- folly/src/main/java/follybot/response/ResponseLogic.java
+++ folly/src/main/java/follybot/response/ResponseLogic.java
@@ -11,7 +11,6 @@ public class ResponseLogic {
 
     public String respond(String question) {
 
-        //alkeellinen
         if (simplified(question).equals("bye")) {
             return "Bye.";
         }
